e44d0e1840ec0d9ac2a9c121a9c9d141eeb5113b
Campbell20 <jcampbell145@alamo.edu>
2020-04-19 09:41:11 -0500

    updated too many dang things!

--- javascript/main.js
+++ javascript/main.js
@@ -2,26 +2,132 @@
 $(document).ready(function () {
     $.getJSON('data/data.json', function (json) {
         createGreenFlight(json);
+        createRedFlight(json);
+        createBlueFlight(json);
+        createBlackFlight(json);
         setJsonData(json);
     });
 
-
-
-
+    //green flight
     function createGreenFlight(json) {
-        var greenTemplate = document.getElementsByTagName("templat-green")[0];
-        var divs = greenTemplate.content.querySelector('div');
+        var greenTemplate = document.getElementsByTagName("template-green")[0];
+        var divs = greenTemplate.querySelector('div');
         var a;
         jsonLength = Object.keys(json).length;
 
         for (i = 0; i < jsonLength; i++) {
             a = document.importNode(divs, true);
             //add them to the page
-            //   document.body.appendChild(a);
-            document.getElementById("green-wingmen").appendChild(a);
+            if (json[i]["Select Fighter Pilot Group:"] == "Detached Fighter Group") {
+                if (json[i]["Select Fighter Pilot Position:"] == "Wingman") {
+                    document.getElementById("green-wingmen").appendChild(a);
+                }
+            }
+            createGreenTeamIds();
         }
     }
 
+    function createGreenTeamIds() {
+        $(".green-number").each(function (i) {
+            $(this).attr('id', "green-number" + i);
+            $(this).text("Green " + (i + 2));
+        });
+        $(".grid-green-pilot").each(function (i) {
+            $(this).attr('id', "green" + i);
+        });
+    };
+
+    //red flight
+    function createRedFlight(json) {
+        var redTemplate = document.getElementsByTagName("template-red")[0];
+        var divs = redTemplate.querySelector('div');
+        var a;
+        jsonLength = Object.keys(json).length - 1;
+
+        for (i = 0; i < jsonLength; i++) {
+            a = document.importNode(divs, true);
+            //add them to the page
+            if (json[i]["Select Fighter Pilot Group:"] == "Escort Fighter Group") {
+                if (json[i]["Select Fighter Pilot Position:"] == "Wingman") {
+                    document.getElementById("red-wingmen").appendChild(a);
+                }
+            }
+            createRedTeamIds();
+        }
+    }
+
+    function createRedTeamIds() {
+        $(".red-number").each(function (i) {
+            $(this).attr('id', "red-number" + i);
+            $(this).text("Red " + (i + 2));
+        });
+        $(".grid-red-pilot").each(function (i) {
+            $(this).attr('id', "red" + i);
+        });
+    };
+
+    //blue flight
+    function createBlueFlight(json) {
+        var blueTemplate = document.getElementsByTagName("template-blue")[0];
+        var divs = blueTemplate.querySelector('div');
+        var a;
+        jsonLength = Object.keys(json).length;
+        attacker = 0
+
+        for (i = 0; i < jsonLength; i++) {
+            a = document.importNode(divs, true);
+            //add them to the page
+            if (json[i]["Select Attacker Pilot Position:"] == "Wingman") {
+                attacker++;
+                if (attacker < 5) {
+                    document.getElementById("blue-wingmen").appendChild(a);
+                   
+                }
+            }
+            createBlueTeamIds();
+        }
+    }
+
+    function createBlueTeamIds() {
+        $(".blue-number").each(function (i) {
+            $(this).attr('id', "blue-number" + i);
+            $(this).text("Blue " + (i + 2));
+        });
+        $(".grid-blue-pilot").each(function (i) {
+            $(this).attr('id', "blue" + i);
+        });
+    };
+
+    //black flight
+    function createBlackFlight(json) {
+        var blackTemplate = document.getElementsByTagName("template-black")[0];
+        var divs = blackTemplate.querySelector('div');
+        var a;
+        jsonLength = Object.keys(json).length;
+        attacker = 0
+        for (i = 0; i < jsonLength; i++) {
+            a = document.importNode(divs, true);
+            //add them to the page
+            if (json[i]["Select Attacker Pilot Position:"] == "Wingman") {
+                attacker++
+                if(attacker >= 6){
+                    document.getElementById("black-wingmen").appendChild(a);
+                   
+                }
+            }
+            createBlackTeamIds();
+        }
+    }
+
+    function createBlackTeamIds() {
+        $(".black-number").each(function (i) {
+            $(this).attr('id', "black-number" + (i  + 6));
+            $(this).text("Black " + (i + 2));
+        });
+        $(".grid-black-pilot").each(function (i) {
+            $(this).attr('id', "black" + i);
+        });
+    };
 
     function setJsonData(json) {
         greenLeaderFilled = false;
@@ -36,7 +142,6 @@ $(document).ready(function () {
         blackLeaderName = "";
         attackerTeam = 0;
 
-        console.log(json);
         jsonLength = Object.keys(json).length;
         for (i = 0; i < jsonLength; i++) {
             if (json[i]["Select role:"] == "Fighter Pilot") {
@@ -45,15 +150,14 @@ $(document).ready(function () {
                         && greenLeaderFilled == false) {
                         greenLeaderFilled = true;
                         greenLeaderName = json[i]["Enter Pilot Name:"];
-                        document.getElementById("green-1").innerHTML = json[i]["Enter Pilot Name:"];
+                        document.getElementById('green0').innerHTML = greenLeaderName;
                     }
-                    else if (greenLeaderName != json[i]["Enter Pilot Name:"]
-                        && greenTeam < 3) {
+                    else if (greenLeaderName != json[i]["Enter Pilot Name:"]) {
                         greenTeam++;
-                        document.getElementById("green-" + (greenTeam + 1)).innerHTML = json[i]["Enter Pilot Name:"];
+                        document.getElementById("green" + (greenTeam)).innerHTML = json[i]["Enter Pilot Name:"];
                     }
                     else {
-                        console.log("To many green team members.");
+                        console.log("Something went wrong with green team!");
                     }
                 }
                 if (json[i]["Select Fighter Pilot Group:"] == "Escort Fighter Group") {
@@ -61,46 +165,44 @@ $(document).ready(function () {
                         && redLeaderFilled == false) {
                         redLeaderFilled = true;
                         redLeaderName = json[i]["Enter Pilot Name:"];
-                        document.getElementById("red-1").innerHTML = json[i]["Enter Pilot Name:"];
+                        document.getElementById("red0").innerHTML = redLeaderName;
                     }
-                    else if (redLeaderName != json[i]["Enter Pilot Name:"]
-                        && redTeam < 3) {
+                    else if (redLeaderName != json[i]["Enter Pilot Name:"]) {
                         redTeam++;
-                        document.getElementById("red-" + (redTeam + 1)).innerHTML = json[i]["Enter Pilot Name:"];
+                        document.getElementById("red" + (redTeam)).innerHTML = json[i]["Enter Pilot Name:"];
                     }
                     else {
-                        console.log("To many red guys.");
+                        console.log("Something went wrong with red team!");
                     }
                 }
             }
             else if (json[i]["Select role:"] == "Attacker Pilot") {
                 if (json[i]["Select Attacker Pilot Position:"] == "Flight Lead"
                     && blueLeaderFilled == false) {
-                    blueLeaderFilled == true;
+                    blueLeaderFilled = true;
                     blueLeaderName = json[i]["Enter Pilot Name:"];
-                    document.getElementById("blue-1").innerHTML = json[i]["Enter Pilot Name:"];
+                    document.getElementById("blue0").innerHTML = blueLeaderName;
                 }
                 else if (json[i]["Select Attacker Pilot Position:"] == "Flight Lead"
                     && blueLeaderFilled
                     && blackLeaderFilled == false) {
-                    blackLeaderFilled == true;
+                    blackLeaderFilled = true;
                     blackLeaderName = json[i]["Enter Pilot Name:"];
-                    document.getElementById("black-1").innerHTML = json[i]["Enter Pilot Name:"];
+                    document.getElementById("black0").innerHTML = blackLeaderName;
                 }
                 else if (json[i]["Select Attacker Pilot Position:"] == "Wingman"
                     && blueLeaderName != json[i]["Enter Pilot Name:"]
-                    && blackLeaderName != json[i]["Enter Pilot Name:"]
-                    && attackerTeam < 6) {
+                    && blackLeaderName != json[i]["Enter Pilot Name:"]) {
                     attackerTeam++;
-                    if (attackerTeam < 3) {
-                        document.getElementById("blue-" + (attackerTeam + 1)).innerHTML = json[i]["Enter Pilot Name:"];
+                    if (attackerTeam < 6) {
+                        document.getElementById("blue" + (attackerTeam)).innerHTML = json[i]["Enter Pilot Name:"];
                     }
                     else {
-                        document.getElementById("black-" + (attackerTeam + 1)).innerHTML = json[i]["Enter Pilot Name:"];
+                        document.getElementById("black" + (attackerTeam - 5)).innerHTML = json[i]["Enter Pilot Name:"];
                     }
                 }
                 else {
-                    console.log("To many attacker dudes.");
+                    console.log("Something went haywire!");
                 }
             }
         }
