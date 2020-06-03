52a0112683a62729d385302f8c0648ab3b17f626
Campbell20 <jcampbell145@alamo.edu>
2020-06-02 19:57:42 -0500

    update so much stuff!

--- javascript/pwcgfnf.js
+++ javascript/pwcgfnf.js
@@ -1,12 +1,36 @@
 
 $(document).ready(function () {
-    $.getJSON('data/pwcgfnfdata.json', function (json) {
-        console.log(json);
+    $.getJSON('https://spreadsheets.google.com/feeds/list/1C4ke63VpXK0yZxbGX1AE03Rf7qF_8t90-M0Yv6QzJzE/ootif9z/public/values?alt=json', function (json) {
+        // console.log(json);
         createGridsFromTemplate(json);
     });
 
+    $.get('/data/Names/FirstNamesUSA.txt', function (fnUSA) {
+        firstNamesUSA = fnUSA.split('\n');
+    });
+    $.get('/data/Names/LastNamesUSA.txt', function (lnUSA) {
+        lastNamesUSA = lnUSA.split('\n');
+    });
+    $.get('/data/Names/FirstNamesGermany.txt', function (fnGermany) {
+        firstNamesGermany = fnGermany.split('\n');
+    });
+    $.get('/data/Names/LastNamesGermany.txt', function (lnGermany) {
+        lastNamesGermany = lnGermany.split('\n');
+    });
+
+
     function createGridsFromTemplate(json) {
-        jsonLength = Object.keys(json).length;
+        jsonLength = Object.keys(json.feed.entry).length;
+        // console.log(firstNamesUSA);
+        //allied planes
+        alliedPlaneA = "P51";
+        alliedPlaneB = "P38";
+        alliedBomber = "A20";
+        //axis planes
+        axisPlaneA = "Fw190A8";
+        axisPlaneB = "Bf109G14";
+        axisBomber = "Ju88";
+
         var alliedFGATemplate = document.getElementsByTagName("template-alliedFGA")[0];
         var alliedFGBTemplate = document.getElementsByTagName("template-alliedFGB")[0];
         var alliedBomberTemplate = document.getElementsByTagName("template-alliedBG")[0];
@@ -28,131 +52,139 @@ $(document).ready(function () {
         axisFGB = 0;
         axisBGLeaderFilled = false;
         axisB = 0;
-
         for (i = 0; i < jsonLength; i++) {
-            if (json[i]["Side"] == "Allied") {
-                if (json[i]["Allied FG"] == "352nd Fighter Group - P51 Mustang") {
+            side = json.feed.entry[i].gsx$side.$t;
+            imageLink = json.feed.entry[i].gsx$image.$t;
+            alliedFG = json.feed.entry[i].gsx$alliedfg.$t;
+            alliedName = firstNamesUSA[imageLink] + " \"" + json.feed.entry[i].gsx$alliedname.$t + "\" " + lastNamesUSA[imageLink];            
+            alliedPosition = json.feed.entry[i].gsx$alliedposition.$t;
+            axisFG = json.feed.entry[i].gsx$axisfg.$t;
+            axisName = firstNamesGermany[imageLink] + " \"" + json.feed.entry[i].gsx$axisname.$t + "\" " + lastNamesGermany[imageLink]; 
+            axisPosition = json.feed.entry[i].gsx$axisposition.$t;
+            
+            if (side == "Allied") {
+                if (alliedFG.includes(alliedPlaneA)) {
                     var alliedFGADivs = alliedFGATemplate.querySelector('div');
                     var alliedfightergroupAD;
 
-                    if (json[i]["Allied Position"] == "Flight Lead" &&
-                    !alliedFGALeaderFilled) {
-                        alliedFGALeaderFilled = true; 
-                        document.getElementById('alliedfighterA-leader').innerHTML = json[i]["Allied Name"];
-                        document.getElementById('alliedfighterA-leader-pic').src = 'imgs/American/American' + json[i]["Image"] + ".jpg";
+                    if (alliedPosition.includes("Lead") &&
+                        !alliedFGALeaderFilled) {
+                        alliedFGALeaderFilled = true;
+                        document.getElementById('alliedfighterA-leader').innerHTML = alliedName;
+                        document.getElementById('alliedfighterA-leader-pic').src = 'imgs/American/American' + imageLink + ".jpg";
                     }
                     else {
                         alliedfightergroupAD = document.importNode(alliedFGADivs, true);
                         document.getElementById("alliedfighterA-wingmen").appendChild(alliedfightergroupAD);
                         createIds();
                         alliedFGA++;
-                        document.getElementById("alliedFGA" + alliedFGA).innerHTML = json[i]["Allied Name"];
-                        document.getElementById('alliedFGA-pic' + alliedFGA).src = 'imgs/American/American' + json[i]["Image"] + ".jpg";
+                        document.getElementById("alliedFGA" + alliedFGA).innerHTML = alliedName;
+                        document.getElementById('alliedFGA-pic' + alliedFGA).src = 'imgs/American/American' + imageLink + ".jpg";
                     }
                 }
-                else if (json[i]["Allied FG"] == "474th Fighter Group - P38 Lightings" ) {
+                else if (alliedFG.includes(alliedPlaneB)) {
                     var alliedFGBDivs = alliedFGBTemplate.querySelector('div');
                     var alliedfightergroupBD;
 
-                    if (json[i]["Allied Position"] == "Flight Lead" &&
-                    !alliedFGBLeaderFilled) {
+                    if (alliedPosition.includes("Lead") &&
+                        !alliedFGBLeaderFilled) {
                         alliedFGBLeaderFilled = true;
-                        document.getElementById('alliedfighterB-leader').innerHTML = json[i]["Allied Name"];
-                        document.getElementById('alliedfighterB-leader-pic').src = 'imgs/American/American' + json[i]["Image"] + ".jpg";
+                        document.getElementById('alliedfighterB-leader').innerHTML = alliedName;
+                        document.getElementById('alliedfighterB-leader-pic').src = 'imgs/American/American' + imageLink + ".jpg";
                     }
                     else {
                         alliedfightergroupBD = document.importNode(alliedFGBDivs, true);
                         document.getElementById("alliedfighterB-wingmen").appendChild(alliedfightergroupBD);
                         createIds();
                         alliedFGB++;
-                        document.getElementById("alliedFGB" + alliedFGB).innerHTML = json[i]["Allied Name"];
-                        document.getElementById('alliedFGB-pic' + alliedFGB).src = 'imgs/American/American' + json[i]["Image"] + ".jpg";
+                        document.getElementById("alliedFGB" + alliedFGB).innerHTML = alliedName;
+                        document.getElementById('alliedFGB-pic' + alliedFGB).src = 'imgs/American/American' + imageLink + ".jpg";
                     }
                 }
                 else {
                     var alliedBomberDivs = alliedBomberTemplate.querySelector('div');
                     var alliedBomberD;
 
-                    if (json[i]["Allied Position"] == "Flight Lead" &&
-                    !alliedBGLeaderFilled) {
+                    if (alliedPosition.includes("Lead") &&
+                        !alliedBGLeaderFilled) {
                         alliedBGLeaderFilled = true;
-                        document.getElementById('alliedbomber-leader').innerHTML = json[i]["Allied Name"];
-                        document.getElementById('alliedbomber-leader-pic').src = 'imgs/American/American' + json[i]["Image"] + ".jpg";
+                        document.getElementById('alliedbomber-leader').innerHTML = alliedName;
+                        document.getElementById('alliedbomber-leader-pic').src = 'imgs/American/American' + imageLink + ".jpg";
                     }
                     else {
                         alliedBomberD = document.importNode(alliedBomberDivs, true);
                         document.getElementById("alliedbomber-wingmen").appendChild(alliedBomberD);
                         createIds();
                         alliedB++;
-                        document.getElementById("alliedBG" + alliedB).innerHTML = json[i]["Allied Name"];
-                        document.getElementById('alliedBG-pic' + alliedB).src = 'imgs/American/American' + json[i]["Image"] + ".jpg";
+                        document.getElementById("alliedBG" + alliedB).innerHTML = alliedName;
+                        document.getElementById('alliedBG-pic' + alliedB).src = 'imgs/American/American' + imageLink + ".jpg";
                     }
                 }
             }
 
             //AXIS SIDE
-            if (json[i]["Side"] == "Axis") {
-                if (json[i]["Axis FG"] == "I./JG77 - Fw190A8 (Fighter Group)") {
+            if (side == "Axis") {
+                if (axisFG.includes(axisPlaneA)) {
                     var axisFGADivs = axisFGATemplate.querySelector('div');
                     var axisfightergroupAD;
 
-                    if (json[i]["Axis Position"] == "Flight Lead" &&
-                    !axisFGALeaderFilled) {
+                    if (axisPosition.includes("Lead") &&
+                        !axisFGALeaderFilled) {
                         axisFGALeaderFilled = true;
-                        document.getElementById('axisfighterA-leader').innerHTML = json[i]["Axis Name"];
-                        document.getElementById('axisfighterA-leader-pic').src = 'imgs/German/German' + json[i]["Image"] + ".jpg";
+                        document.getElementById('axisfighterA-leader').innerHTML = axisName;
+                        document.getElementById('axisfighterA-leader-pic').src = 'imgs/German/German' + imageLink + ".jpg";
                     }
                     else {
                         axisfightergroupAD = document.importNode(axisFGADivs, true);
                         document.getElementById("axisfighterA-wingmen").appendChild(axisfightergroupAD);
                         createIds();
                         axisFGA++;
-                        document.getElementById("axisFGA" + axisFGA).innerHTML = json[i]["Axis Name"];
-                        document.getElementById('axisFGA-pic' + axisFGA).src = 'imgs/German/German' + json[i]["Image"] + ".jpg";
+                        document.getElementById("axisFGA" + axisFGA).innerHTML = axisName;
+                        document.getElementById('axisFGA-pic' + axisFGA).src = 'imgs/German/German' + imageLink + ".jpg";
                     }
                 }
-                else if (json[i]["Axis FG"] == "I./JG1 - Bf109G14 (Fighter Group)") {
+                else if (axisFG.includes(axisPlaneB)) {
                     var axisFGBDivs = axisFGBTemplate.querySelector('div');
                     var axisfightergroupBD;
 
-                    if (json[i]["Axis Position"] == "Flight Lead" &&
-                    !axisFGBLeaderFilled) {
+                    if (axisPosition.includes("Lead") &&
+                        !axisFGBLeaderFilled) {
                         axisFGBLeaderFilled = true;
-                        document.getElementById('axisfighterB-leader').innerHTML = json[i]["Axis Name"];
-                        document.getElementById('axisfighterB-leader-pic').src = 'imgs/German/German' + json[i]["Image"] + ".jpg";
+                        document.getElementById('axisfighterB-leader').innerHTML = axisName;
+                        document.getElementById('axisfighterB-leader-pic').src = 'imgs/German/German' + imageLink + ".jpg";
                     }
                     else {
                         axisfightergroupBD = document.importNode(axisFGBDivs, true);
                         document.getElementById("axisfighterB-wingmen").appendChild(axisfightergroupBD);
                         createIds();
                         axisFGB++;
-                        document.getElementById("axisFGB" + axisFGB).innerHTML = json[i]["Axis Name"];
-                        document.getElementById('axisFGB-pic'+ axisFGB).src = 'imgs/German/German' + json[i]["Image"] + ".jpg";
+                        document.getElementById("axisFGB" + axisFGB).innerHTML = axisName;
+                        document.getElementById('axisFGB-pic' + axisFGB).src = 'imgs/German/German' + imageLink + ".jpg";
                     }
                 }
                 else {
                     var axisBomberDivs = axisBomberTemplate.querySelector('div');
                     var axisBomberD;
 
-                    if (json[i]["Axis Position"] == "Flight Lead" &&
-                    !axisBGLeaderFilled) {
+                    if (axisPosition.includes("Lead") &&
+                        !axisBGLeaderFilled) {
                         axisBGLeaderFilled = true;
-                        document.getElementById('axisbomber-leader').innerHTML = json[i]["Axis Name"];
-                        document.getElementById('axisbomber-leader-pic').src = 'imgs/German/German' + json[i]["Image"] + ".jpg";
+                        document.getElementById('axisbomber-leader').innerHTML = axisName;
+                        document.getElementById('axisbomber-leader-pic').src = 'imgs/German/German' + imageLink + ".jpg";
                     }
                     else {
                         axisBomberD = document.importNode(axisBomberDivs, true);
                         document.getElementById("axisbomber-wingmen").appendChild(axisBomberD);
                         createIds();
                         axisB++;
-                        document.getElementById("axisBG" + axisB).innerHTML = json[i]["Axis Name"];
-                        document.getElementById('axisBG-pic' + axisB).src = 'imgs/German/German' + json[i]["Image"] + ".jpg";
+                        document.getElementById("axisBG" + axisB).innerHTML = axisName;
+                        document.getElementById('axisBG-pic' + axisB).src = 'imgs/German/German' + imageLink + ".jpg";
                     }
                 }
             }
-
         }
     }
+
 
     function createIds() {
         $(".alliedFGA-number").each(function (i) {
