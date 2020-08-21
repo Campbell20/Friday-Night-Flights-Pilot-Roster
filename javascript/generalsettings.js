0eb3d05e6b264b0ec4516bf930ab62c8d5d38546
Campbell20 <jcampbell145@alamo.edu>
2020-08-21 17:01:14 -0500

    turned registration off

--- javascript/generalsettings.js
+++ javascript/generalsettings.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     axisBomber = "BF110G2";
 
     eventDate = "August 21st";
-    registration = true;
+    registration = false;
 
 
     function setFlightsAndPlanes() {
