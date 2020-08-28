d428dab0393e65eebe461714b98c6efb8e1e4994
Campbell20 <jcampbell145@alamo.edu>
2020-08-28 10:23:19 -0500

    close reg

--- javascript/generalsettings.js
+++ javascript/generalsettings.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     axisBomber = "BF110G2";
 
     eventDate = "August 28th";
-    registration = true;
+    registration = false;
 
 
     function setFlightsAndPlanes() {
