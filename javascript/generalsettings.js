b4f170452b93753486c5c8da41fb3246b43ceb74
Campbell20 <jcampbell145@alamo.edu>
2020-09-18 15:23:48 -0500

    locked registration

--- javascript/generalsettings.js
+++ javascript/generalsettings.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     axisBomber = "BF110G2";
 
     eventDate = "September 18th";
-    registration = true;
+    registration = false;
 
 
     function setFlightsAndPlanes() {
