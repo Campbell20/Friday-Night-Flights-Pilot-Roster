bbfe185c1b2d838d59b4a2c6f5cb66164253cfc9
Campbell20 <jcampbell145@alamo.edu>
2021-03-19 12:09:53 -0500

    registration closed

--- javascript/fnfmain.js
+++ javascript/fnfmain.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     axisBomber = "Bf110G2";
 
     eventDate = "<em>Jugs vs Messers</em> - <strong>March 19th, 2021 at 9pm Eastern</strong>";
-    registration = true;
+    registration = false;
 
     $.getJSON('https://spreadsheets.google.com/feeds/list/1L3xLMrObQItYs0vnazhZK06TAaIGamsxSBMaMOCffv4/1/public/full?alt=json').done(function (json) {
         //side names
