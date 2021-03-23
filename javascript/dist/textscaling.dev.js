b558f6b182ddce402111ff7810d1d2dc73056780
Campbell20 <jcampbell145@alamo.edu>
2021-03-23 04:50:26 -0500

    modal mostly mobile

--- javascript/dist/textscaling.dev.js
+++ javascript/dist/textscaling.dev.js
@@ -6,22 +6,29 @@ $(document).ready(function () {
     $(".squadron-leader").text("Squad Lead");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "350";
+  } else if ($(window).width() > 510 && $(window).width() <= 990) {
+    document.getElementById("google-iframe").width = "450";
   } else if ($(window).width() > 991 && $(window).width() <= 1209) {
     $(".squadron-leader").text("SL");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "700";
   } else if ($(window).width() >= 1210 && $(window).width() <= 1529) {
     $(".squadron-leader").text("SL");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "700";
   } else if ($(window).width() >= 1530 && $(window).width() <= 1950) {
     $(".squadron-leader").text("Squad Lead");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "700";
   } else {
     $(".squadron-leader").text("Squadron Leader");
     $(".grid-subtitle-assignment").text("Assignment");
     $(".grid-subtitle-name").text("Pilot Name");
+    document.getElementById("google-iframe").width = "700";
   }
 });
 $(window).resize(function () {
@@ -30,21 +37,28 @@ $(window).resize(function () {
     $(".squadron-leader").text("Squad Lead");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "350";
+  } else if ($(window).width() > 510 && $(window).width() <= 990) {
+    document.getElementById("google-iframe").width = "450";
   } else if ($(window).width() > 991 && $(window).width() <= 1209) {
     $(".squadron-leader").text("SL");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "700";
   } else if ($(window).width() >= 1210 && $(window).width() <= 1529) {
     $(".squadron-leader").text("SL");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "700";
   } else if ($(window).width() >= 1530 && $(window).width() <= 1950) {
     $(".squadron-leader").text("Squad Lead");
     $(".grid-subtitle-assignment").text("Wing");
     $(".grid-subtitle-name").text("Name");
+    document.getElementById("google-iframe").width = "700";
   } else {
     $(".squadron-leader").text("Squadron Leader");
     $(".grid-subtitle-assignment").text("Assignment");
     $(".grid-subtitle-name").text("Pilot Name");
+    document.getElementById("google-iframe").width = "700";
   }
 });
--- javascript/textscaling.js
+++ javascript/textscaling.js
@@ -5,22 +5,31 @@ $(document).ready(function () {
         $(".squadron-leader").text("Squad Lead");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
-    } else if ($(window).width() > 991 && $(window).width() <= 1209) {
+        document.getElementById("google-iframe").width = "350";
+    } 
+    else if ($(window).width() > 510 && $(window).width() <= 990){
+        document.getElementById("google-iframe").width = "450";
+    }
+    else if ($(window).width() > 991 && $(window).width() <= 1209) {
         $(".squadron-leader").text("SL");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
+        document.getElementById("google-iframe").width = "700";
     } else if ($(window).width() >= 1210 && $(window).width() <= 1529) {
         $(".squadron-leader").text("SL");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
+        document.getElementById("google-iframe").width = "700";
     } else if ($(window).width() >= 1530 && $(window).width() <= 1950) {
         $(".squadron-leader").text("Squad Lead");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
+        document.getElementById("google-iframe").width = "700";
     } else {
         $(".squadron-leader").text("Squadron Leader");
         $(".grid-subtitle-assignment").text("Assignment");
         $(".grid-subtitle-name").text("Pilot Name");
+        document.getElementById("google-iframe").width = "700";
     }
 });
 
@@ -31,21 +40,30 @@ $(window).resize(function () {
         $(".squadron-leader").text("Squad Lead");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
-    } else if ($(window).width() > 991 && $(window).width() <= 1209) {
+        document.getElementById("google-iframe").width = "350";
+    } 
+    else if ($(window).width() > 510 && $(window).width() <= 990){
+        document.getElementById("google-iframe").width = "450";
+    }
+    else if ($(window).width() > 991 && $(window).width() <= 1209) {
         $(".squadron-leader").text("SL");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
+        document.getElementById("google-iframe").width = "700";
     } else if ($(window).width() >= 1210 && $(window).width() <= 1529) {
         $(".squadron-leader").text("SL");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
+        document.getElementById("google-iframe").width = "700";
     } else if ($(window).width() >= 1530 && $(window).width() <= 1950) {
         $(".squadron-leader").text("Squad Lead");
         $(".grid-subtitle-assignment").text("Wing");
         $(".grid-subtitle-name").text("Name");
+        document.getElementById("google-iframe").width = "700";
     } else {
         $(".squadron-leader").text("Squadron Leader");
         $(".grid-subtitle-assignment").text("Assignment");
         $(".grid-subtitle-name").text("Pilot Name");
+        document.getElementById("google-iframe").width = "700";
     }
 });
