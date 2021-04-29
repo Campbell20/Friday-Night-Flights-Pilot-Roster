13f4fd6ac5660032e8a50328515c85b5fa2ab004
Campbell20 <jcampbell145@alamo.edu>
2021-04-29 10:58:06 -0500

    removed cache control

--- javascript/dist/sefmain.dev.js
+++ javascript/dist/sefmain.dev.js
@@ -182,4 +182,11 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+} //Removes cachce control
+
+
+response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+
+response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+
+response.setHeader("Expires", "0"); // Proxies.
