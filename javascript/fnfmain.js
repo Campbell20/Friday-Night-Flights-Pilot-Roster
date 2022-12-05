f285f86b9cb84ea0c549acc96c98fbe244538b79
Campbell20 <jcampbell145@alamo.edu>
2022-12-04 22:07:42 -0600

    updated google sheet data

--- javascript/fnfmain.js
+++ javascript/fnfmain.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
-  $.getJSON('https://spreadsheets.google.com/feeds/list/1L3xLMrObQItYs0vnazhZK06TAaIGamsxSBMaMOCffv4/1/public/full?alt=json').done(function (json) {
+  $.getJSON('https://spreadsheets.google.com/feeds/cells/1L3xLMrObQItYs0vnazhZK06TAaIGamsxSBMaMOCffv4/1/public/full?alt=json').done(function (json) {
+    //$.getJSON('https://spreadsheets.google.com/feeds/list/1L3xLMrObQItYs0vnazhZK06TAaIGamsxSBMaMOCffv4/1/public/full?alt=json').done(function (json) {
     //side names
     alliedSide = "Allied";
     axisSide = "Axis";
